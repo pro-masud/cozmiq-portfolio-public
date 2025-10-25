61d3c20a886c5906793f0fa5a63f19180a0eb3bf
pro-masud <promasudbd@gmail.com>
2025-10-25 16:45:52 +0600

    feat: portfolio one section complete

--- portfolio-cozmiq/assets/js/main.js
+++ portfolio-cozmiq/assets/js/main.js
@@ -1,6 +1,29 @@
 (function($) {
     $(document).ready(function() {
         
-        // Write To Jquery Code......
+         // Smooth, continuous, “infinite” marquee feel
+        const swiper = new Swiper('#portfolioSwiper', {
+            loop: true,                      
+            slidesPerView: 3,          
+            spaceBetween: '30px',
+            freeMode: {
+                enabled: true,
+                momentum: false,             
+            },
+            speed: 6000,                   
+            autoplay: {
+                delay: 0,                    
+                disableOnInteraction: false,
+                pauseOnMouseEnter: true
+            },
+            allowTouchMove: true,    
+            grabCursor: true,
+        });
+
+        swiper.on('touchEnd', () => {
+            swiper.params.autoplay.delay = 0;
+            swiper.autoplay.start();
+        });
+
     });
 })(jQuery);
